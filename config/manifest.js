56df6ae8fd87fd80a81117d9e8b367754737d143
Trent Stromkins <trent@smallrobot.org>
2017-12-01 15:06:19 -0800

    splash assets

--- config/manifest.js
+++ config/manifest.js
@@ -14,6 +14,44 @@ module.exports = function(/* environment, appConfig */) {
     background_color: "#fff",
     theme_color: "#fff",
     icons: [
-    ]
+      {
+        src: '/assets/icons/appicon-32.png',
+        sizes: `32x32`,
+        targets: ['favicon']
+      },
+      ...[192, 280, 512].map((size) => ({
+        src: `/assets/icons/appicon-${size}.png`,
+        sizes: `${size}x${size}`
+      }))
+      // {
+      //   src: "/img/icons/android-icon-192x192.png",
+      //   sizes: "192x192",
+      //   type: "image/png"
+      // },
+      // {
+      //   src: "/img/icons/android-icon-512x512.png",
+      //   sizes: "512x512",
+      //   type: "image/png"
+      // },
+      // {
+      //   src: "/img/icons/apple-icon-180x180.png",
+      //   sizes: "180x180",
+      //   type: "image/png",
+      //   targets: ['apple']
+      // },
+      // {
+      //   src: "/img/icons/ms-icon-150x150.png",
+      //   element: "150x150",
+      //   targets: ['ms']
+      // }
+    ],
+
+    apple: {
+      statusBarStyle: 'black-translucent'
+    },
+
+    ms: {
+      tileColor: '#ffffff'
+    }
   };
 }
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,24 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    'ember-cli-image-transformer': {
+      images: [
+        {
+          inputFilename: 'public/img/icon.png',
+          outputFileName: 'appicon-',
+          convertTo: 'png',
+          destination: 'assets/icons/',
+          sizes: [32, 192, 280, 512]
+        },
+        {
+          inputFilename: 'public/img/splash.png',
+          outputFileName: 'appsplash-',
+          convertTo: 'png',
+          destination: 'assets/splashes/',
+          sizes: [512]
+        }
+      ]
+    },
     'asset-cache': {
       exclude: [
         'img/**/*',
