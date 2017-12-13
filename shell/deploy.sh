604bcbf028e30a72de5be3743e178314f06bb6be
Trent Stromkins <trent@smallrobot.org>
2017-12-13 12:28:45 -0800

    shell script

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERNAME=root
 HOSTS="server.smallrobot.org"
-SCRIPT="cd /home/smallrobot.co/public_html; git pull; logout;"
+SCRIPT="cd /home/smallrobot.co/public_html; git pull;"
 
 for HOSTNAME in ${HOSTS} ; do
     ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
