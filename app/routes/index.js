d984a6b86f174c1a81d0600ef1351a54e43865e1
Trent Stromkins <trent@smallrobot.org>
2018-01-21 14:32:25 -0800

    model shuffle

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,10 +6,12 @@ export default Route.extend({
   model() {
     return RSVP.hash({
       page: this.store.findRecord('page', '93aec22a-3710-4fe0-ae09-663f6790bb79'),
+      texts: this.store.findAll('text'),
     });
   },
 
   setupController(controller, models) {
     controller.set('page', models.page);
+    controller.set('texts', models.texts);
   }
 });
