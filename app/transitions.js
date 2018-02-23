5a46fd7289f2ba9fe96a845fe314b3e43fec6926
Trent Stromkins <trent@smallrobot.org>
2018-02-23 01:41:40 -0800

    loading

--- app/transitions.js
+++ app/transitions.js
@@ -1,17 +1,17 @@
-export default function(){
-  // this.transition(
-  //   this.toRoute(['*']),
-  //   this.use('toLeft'),
-  //   this.reverse('toRight')
-  // );
-  // this.transition(
-  //   this.toRoute(['home','about','consulting','development','support', 'pricing','contact','blog','blog.post','notfound']),
-  //   this.use('fade', { duration: 500 })
-  // );
-  const duration = 500;
-  this.transition(
-    this.use('toUp', { duration })
-  );
-}
+// export default function(){
+//   // this.transition(
+//   //   this.toRoute(['*']),
+//   //   this.use('toLeft'),
+//   //   this.reverse('toRight')
+//   // );
+//   // this.transition(
+//   //   this.toRoute(['home','about','consulting','development','support', 'pricing','contact','blog','blog.post','notfound']),
+//   //   this.use('fade', { duration: 500 })
+//   // );
+//   const duration = 1500;
+//   this.transition(
+//     this.use('toLeft', { duration })
+//   );
+// }
 
 
