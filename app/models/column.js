77c4f91f3d5d231305105bb6dcfc233738d08129
Trent Stromkins <trent@smallrobot.org>
2018-03-23 12:43:33 -0700

    column layout changes

--- app/models/column.js
+++ app/models/column.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  alignment: DS.attr('string'),
   columnWidthXs: DS.attr('string'),
   columnWidthSm: DS.attr('string'),
   columnWidthMd: DS.attr('string'),
