325f4f274f015515515459996fdb4cfb6c93fba7
Trent Stromkins <trent@smallrobot.org>
2018-05-03 12:05:06 -0700

    fastboot initializers

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,12 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
+  fullName: DS.attr('string'),
+  organization: DS.attr('string'),
   email: DS.attr('string'),
-  aboutMe: DS.attr('string'),
-  budget: DS.attr('number'),
-  messageBody: DS.attr('string'),
-  messageType: DS.attr('string')
+  phone: DS.attr('string'),
+  message: DS.attr('string')
 });
