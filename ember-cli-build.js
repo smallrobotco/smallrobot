386a46b0b4b9fcbcb5d87095c84ca0c17a52f235
Trent Stromkins <trent@smallrobot.org>
2018-05-23 14:18:11 -0700

    premeber

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -53,7 +53,7 @@ module.exports = function(defaults) {
         'fonts/**/*',
         'img/**/*'
       ],
-      version: '4',
+      version: '5',
       requestMode: 'cors'
     },
     'esw-cache-first': {
@@ -66,22 +66,22 @@ module.exports = function(defaults) {
       patterns: [
         '/api/(.+)'
       ],
-    } //,
-    // 'esw-prember': {
-    //   version: '4'
-    // },
-    // 'prember': {
-    //   baseRoot: 'https://smallrobot.co',
-    //   urls: [
-    //     '/',
-    //     '/consulting',
-    //     '/development',
-    //     '/support',
-    //     '/contact',
-    //     '/about',
-    //     '/ideas',
-    //   ]
-    // }
+    },
+    'esw-prember': {
+      version: '5'
+    },
+    'prember': {
+      baseRoot: 'https://smallrobot.co',
+      urls: [
+        '/',
+        '/consulting',
+        '/development',
+        '/support',
+        '/contact',
+        '/about',
+        '/ideas',
+      ]
+    }
   });
   return app.toTree();
 };
