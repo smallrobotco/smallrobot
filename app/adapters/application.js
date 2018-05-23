be2c5671ff6a21f3ce247711421ca87fb7ba4a59
Trent Stromkins <trent@smallrobot.org>
2018-05-23 14:10:09 -0700

    data

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
+import DS from 'ember-data';
 import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
-import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import ENV from '../config/environment';
 
-export default JSONAPIAdapter.extend(AdapterFetch, {
+export default DS.JSONAPIAdapter.extend(AdapterFetch, {
   host: ENV.host,
   namespace: ENV.namespace,
   // buildURL(record, suffix) {
