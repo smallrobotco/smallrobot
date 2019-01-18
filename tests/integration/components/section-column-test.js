4f5ec3d05c0ac3a4454584d639518face4ddc918
Trent Stromkins <trent@smallrobot.org>
2019-01-17 16:21:51 -0800

    update to 3.7.1

--- tests/integration/components/section-column-test.js
+++ tests/integration/components/section-column-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | section-column', function(hooks) {
 
     await render(hbs`{{section-column}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | section-column', function(hooks) {
       {{/section-column}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
